43a318921b361572fd8dfb776489e4465b65bfe7
javier <javierloperez15@gmail.com>
2025-02-06 18:01:06 +0100

    final2?

--- app/src/main/java/com/example/examen2evajavierlopez/ui/pantallas/PantallaInicio.kt
+++ app/src/main/java/com/example/examen2evajavierlopez/ui/pantallas/PantallaInicio.kt
@@ -1,10 +1,35 @@
 
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.example.examen2evajavierlopez.R
 
 
 @Composable
 fun PantallaInicio(modifier: Modifier) {
 
+}
+
+
+@Composable
+fun PantallaCargando(modifier: Modifier) {
+
+    Image(
+        modifier = modifier.fillMaxSize(),
+        painter = painterResource(R.drawable.cargando),
+        contentDescription = stringResource(R.string.cargando)
+    )
+}
+
+@Composable
+fun PantallaError(modifier: Modifier) {
+    Image(
+        modifier = modifier.fillMaxSize(),
+        painter = painterResource(R.drawable.error),
+        contentDescription = stringResource(R.string.error)
+    )
 }
